b462bcd6273173db0aea71883a480214cc65b7d3
Puja Ahuja <ahujapuja55@gmail.com>
2024-02-29 00:40:38 +0530

    JSON

---
+++ src/Data.jsx
@@ -0,0 +1,32 @@
+export const userList = [
+    {
+        id:1,
+        name: "Neha Suryawanshi",
+        email : "nehasur12@gmail.com"
+    },
+    {
+        id:2,
+        name: "Soniya Yadav",
+        email : "soniya34@gmail.com"
+    },
+    {
+        id:3,
+        name: "Kanchan Gaware",
+        email : "kanchan33@gmail.com"
+    },
+    {
+        id:4,
+        name: "Divya Gurav",
+        email : "gurav22@gmail.com"
+    },
+    {
+        id:5,
+        name: "Dhanshree Badwane",
+        email : "dhanuu99@gmail.com"
+    },
+    {
+        id:6,
+        name: "Varsha Jadhav",
+        email : "varshu@gmail.com"
+    }
+]
---
+++ src/Update.jsx
@@ -0,0 +1,54 @@
+import React, {useState} from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux';
+import { updateUser } from './UserReducer';
+
+const Update = () => {
+    const {id} = useParams();
+    const users = useSelector((state) => state.users);
+    const existingUser = users.filter (f=> f.id == id);
+    const {name, email} = existingUser[0];
+    const [uname, setName] = useState(name)
+    const [uemail, setEmail] = useState(email)
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleUpdate = (event) =>{
+        event.preventDefault();
+        dispatch(updateUser({
+            id: id,
+            name: uname,
+            email: uemail
+        }))
+        navigate('/')
+
+    }
+    
+  return (
+    <div className='d-flex w-100 justify-content-center align-items-center'>
+    <br/>
+    <div className='w-50 border bg-secondary text-white p-5'>
+    <h3>Add new user</h3>
+    <form onSubmit={handleUpdate}>
+    <div>
+    <label htmlFor="name">Name:</label>
+    <input type="text" name="name" className='form-control' placeholder='Enter name' value={uname}
+    onChange={e=> setName(e.target.value)}/>
+    </div>
+
+    <div>
+    <label htmlFor="email">Email:</label>
+    <input type="email" name="email" className='form-control' placeholder='Enter email' value={uemail}
+    onChange={ e=>setEmail(e.target.value)}/>
+    </div>
+    <br/>
+
+    <button className='btn btn-info'>Update</button>
+
+    </form>
+    </div>
+    </div>
+  )
+}
+
+export default Update
